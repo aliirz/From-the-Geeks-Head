f8f3c846631937a3a42af3177f8d5f1e21fddfcc
aliirz <dejavu87@gmail.com>
2013-09-12 01:27:34 +0500

    Implementing Kudos

--- app/js/geekshead.js
+++ app/js/geekshead.js
@@ -13,4 +13,69 @@ $(window).resize(function(){
 	if(w > 320 && menu.is(':hidden')) {  
 		menu.removeAttr('style');  
 	}  
-}); 
+});
+
+			// needs to be a string for jquery.cookie
+			var postId = '1'; 
+
+			$(function()
+			{
+				// initialize kudos
+				$("figure.kudoable").kudoable();
+
+				// check to see if user has already kudod
+				// fyi cookies do not work when you are viewing this as a file
+				if($.cookie(postId) == 'true') {
+					// make kudo already kudod
+					$("figure.kudoable").removeClass("animate").addClass("complete");
+
+					// your server would take care of the proper kudos count, but because this is a
+					// static page, we need to set it here so it doesn't become -1 when you remove
+					// the kudos after a reload
+					$(".num").html(1);
+				}	
+
+				// when kudoing
+				$("figure.kudo").bind("kudo:active", function(e)
+				{
+					console.log("kudoing active");
+				});
+
+				// when not kudoing
+				$("figure.kudo").bind("kudo:inactive", function(e)
+				{
+					console.log("kudoing inactive");
+				});
+
+				// after kudo'd
+				$("figure.kudo").bind("kudo:added", function(e)
+				{
+					var element = $(this);
+					// ajax'y stuff or whatever you want
+					console.log("Kodo'd:", element.data('id'), ":)");
+					$("#myModal").modal();
+					// set cookie so user cannot kudo again for 7 days
+					//$.cookie(postId, 'true', { expires: 7 });
+				});
+
+				// after removing a kudo
+				$("figure.kudo").bind("kudo:removed", function(e)
+				{
+					var element = $(this);
+					// ajax'y stuff or whatever you want
+					console.log("Un-Kudo'd:", element.data('id'), ":(");
+
+					// remove cookie
+					//$.removeCookie(postId);
+				});
+			});
+
+
+// bind to events on the kudos
+$("figure.kudo").on("kudo:added", function(event)
+{
+  var element = $(this);
+  var id = element.data('id');
+  // send the data to your server...
+  console.log("Kudod", element);
+});
